a04ec52ae08053970ceec8dd5c81ff1dd5d7674e
developerv2000 <developerv2000@gmail.com>
2024-01-22 11:17:08 +0500

    Webpack files rebuilded

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,18 @@
-// Register Service worker for PWA
-if ('serviceWorker' in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register(document.location.origin + '/service-worker.js').then(function (registration) {
-        console.log('Service worker registration succeeded:', registration);
-    }, /*catch*/ function (error) {
-        console.log('Service worker registration failed:', error);
-    });
-} else {
-    console.log('Service workers are not supported.');
-}
-
-
 // initialize components
 $(document).ready(function () {
+    // Register Service worker for PWA
+    if ('serviceWorker' in navigator) {
+        // Register a service worker hosted at the root of the
+        // site using the default scope.
+        navigator.serviceWorker.register(document.location.origin + '/service-worker.js').then(function (registration) {
+            console.log('Service worker registration succeeded:', registration);
+        }, /*catch*/ function (error) {
+            console.log('Service worker registration failed:', error);
+        });
+    } else {
+        console.log('Service workers are not supported.');
+    }
+
     $('.selectize-singular').selectize({
         // options
     });
